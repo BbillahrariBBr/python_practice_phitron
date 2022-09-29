7d57b6226b5d067264146740b725c3a18dd60f2a
BbillahrariBBr <mb.billah8@gmail.com>
2022-09-30 00:36:49 +0600

    input not end

---
+++ Module-01/input.py
@@ -0,0 +1,2 @@
+age = input("Tell me your age: ")
+print(f'your age is {age} type {type(age)}')
---
+++ Module-01/type.py
@@ -0,0 +1,15 @@
+weight = 58
+print(type(weight))
+
+price = 13.73
+print(type(price))
+
+pen = 'Econo'
+print(type(pen))
+
+
+kana = True
+print(type(kana))
+""" f string """
+about_me = f'my name is {pen} price is: {price}'
+print(about_me)
