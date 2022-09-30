bf58b66aa4b903c62176bc034d78164e694c704a
BbillahrariBBr <mb.billah8@gmail.com>
2022-09-30 09:39:43 +0600

    hw 1 and 2 done

---
+++ Module-01/Hw/hw1and2.py
@@ -0,0 +1,7 @@
+from distutils.command.build_scripts import first_line_re
+
+
+first_number = int(input('Enter first Number: '))
+second_number = int(input('Enter second Number: ')) 
+third_number = int(input('Enter third Number: '))
+print(first_number+second_number+third_number)
