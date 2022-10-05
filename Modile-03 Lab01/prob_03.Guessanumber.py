862dc5d5b50c01215a9a0492593d37e5a98ba082
BbillahrariBBr <mb.billah8@gmail.com>
2022-10-06 00:40:16 +0600

    guess a number

---
+++ Modile-03 Lab01/prob_03.Guessanumber.py
@@ -0,0 +1,31 @@
+# prob_03.Guessanumber.py
+""" 
+*user instruction
+*delay
+formula  = ((2x+8)/2)-x
+ """
+from time import sleep
+
+# print("Guess a number: ")
+# sleep(3)
+# print("Multiply 2 with your number: ")
+# sleep(3) 
+# print("Add 8 with the result")
+# sleep(3)             
+# print("divide the whole result with 2")
+# sleep(3)             
+# print("subtract the guessed number with the result")
+# sleep(3)
+# print('ur guessed nmbr is 4')
+
+x = input("Guess a number: ")
+
+x = input("Multiply 2 with your number: ")
+ 
+x = input("Add 8 with the result")
+             
+x = input("divide the whole result with 2")
+             
+x = input("subtract the guessed number with the result")
+
+x = input('ur guessed nmbr is 4')
