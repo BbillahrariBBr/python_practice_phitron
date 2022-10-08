4df58e839734fb027245af4ecb300491dde34034
BbillahrariBBr <mb.billah8@gmail.com>
2022-10-08 23:01:38 +0600

    Simple Introduction To Function In Python And Return

---
+++ Module-04/function.py
@@ -0,0 +1,17 @@
+def double_it(number):
+    result = number*2
+    print(result)
+
+# double_it(6)
+def add(num1,num2):
+    total =num1+num2
+    print(total)
+
+# add(56,23)
+
+def multiply(num1,num2):
+    result = num1*num2
+    return result
+
+output = multiply(25,5)
+print(output)
