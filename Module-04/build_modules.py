caf48a4a2d12089c55169b89a896de54769b96c8
BbillahrariBBr <mb.billah8@gmail.com>
2022-10-09 11:26:34 +0600

    Build In Modules And Build In Functions

---
+++ Module-04/build_modules.py
@@ -0,0 +1,10 @@
+# import math
+from math import ceil, sin 
+import random
+import datetime as dt
+# print(sin(90))
+# print(ceil(4.5))
+num = random.random()
+n = random.randint(70,90)
+winner= random.choice([4,8,9,5,687,56])
+print(winner)
