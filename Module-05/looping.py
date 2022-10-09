91403a4a467d6c68e561213748189994a86dfc20
BbillahrariBBr <mb.billah8@gmail.com>
2022-10-09 22:25:39 +0600

    looping in list set tuple and dictionary

---
+++ Module-05/looping.py
@@ -0,0 +1,19 @@
+num = [12,45,65,23,89,78,11]
+nums = {12,45,65,23,89,78,11}
+marks = {'physics':12, 'chemistry':45,'math':56,'english':89}
+# list index and value using enumarate
+for i,n in enumerate(num):
+    print(i,n) 
+# total = sum(num)
+total = 0
+# for nums in num:
+#     total +=nums
+#     # print(i)
+# print(total)
+# for mark in marks:
+#     val = marks[mark]
+#     print(val)
+
+# for subject, mark in marks.items():
+#     print(subject,mark)
+
