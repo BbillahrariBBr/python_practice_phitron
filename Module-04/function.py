78cd53f4be45f458e11f6e5bec760e2d111d2029
BbillahrariBBr <mb.billah8@gmail.com>
2022-10-09 11:12:22 +0600

    Import Functions Written In Other Files

--- Module-04/function.py
+++ Module-04/function.py
@@ -14,4 +14,4 @@ def multiply(num1,num2):
     return result
 
 output = multiply(25,5)
-print(output)
+# print(output)
---
+++ Module-04/modules.py
@@ -0,0 +1,6 @@
+# from function import add, multiply
+from function import *
+print(add(10,12))
+# import function
+# sum = function.multiply(2,56)
+# print(sum)
