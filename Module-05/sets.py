d9dee4a65c2e56c2b01e5ca9a97f66b53bbc317d
BbillahrariBBr <mb.billah8@gmail.com>
2022-10-09 21:21:54 +0600

    set and tuple in python

---
+++ Module-05/sets.py
@@ -0,0 +1,14 @@
+num = [12,45,56,45,12,89]
+# print(num)
+# set
+nums = {12,45,56,45,12,89}
+# print(nums)
+empty = set()
+num_set = set(num)
+print(num_set)
+num_set.add(77)
+print(num_set)
+num_set.remove(12)
+print(num_set)
+print(len(num_set))
+# print(empty)
---
+++ Module-05/tupple.py
@@ -0,0 +1,11 @@
+num = [12,45,56,45,12,89]
+# print(num)
+num_tuple = 12,45,56,45,12,89
+# print(num_tuple)
+tuple2d = ([12,45,12],[45,11])
+tuple2d[0][2] = 99
+print(tuple2d)
+short_tuple = 2,
+print(short_tuple)
+tuple_frm_list = tuple(num)
+print(tuple_frm_list)
