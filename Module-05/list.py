0a404ec4bed6f0548df40710372d7df1acdeb165
BbillahrariBBr <mb.billah8@gmail.com>
2022-10-09 21:04:33 +0600

    list, list methods and slice

---
+++ Module-05/list.py
@@ -0,0 +1,10 @@
+num = [12,45,65,23,89,78,11]
+# print(num[0])
+# print(num[-6])
+# print(num[1:5])
+# print(num[1:5:2])
+# print(num[3:])
+# list[start:end:step]
+# print(num[5:1:-2])
+# print(num[:])
+print(num[::-1]) # reverse list
