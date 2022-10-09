faba9bc5eb8dd569e9a3d813d71bb31cf3d77c75
BbillahrariBBr <mb.billah8@gmail.com>
2022-10-09 21:54:13 +0600

    dictionaty in python

---
+++ Module-05/dict.py
@@ -0,0 +1,10 @@
+marks = {'physics':12, 'chemistry':45,'math':56}
+marks['math'] = 56.5
+marks['english'] =89
+# del marks['chemistry']
+# print(marks)
+marks_key = marks.keys()
+marks_val  = marks.values()
+marks_item = marks.items()
+# marks.clear()
+print(marks_item)
