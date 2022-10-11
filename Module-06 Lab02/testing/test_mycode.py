7e6b4dee306d791a682d369d8bdb17cceffff370
BbillahrariBBr <mb.billah8@gmail.com>
2022-10-12 01:14:18 +0600

    Testing Codep

---
+++ Module-06 Lab02/testing/test_mycode.py
@@ -0,0 +1,10 @@
+""" 
+test_mycode.py
+
+ """
+import my_code 
+api_url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY"
+
+def test_getresponse():
+    ret = my_code.get_response(api_url)
+    assert ret.status_code == 200
