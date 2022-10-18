fba75cb5d54739b6ab14ff3e4b12f00f7f355df3
BbillahrariBBr <mb.billah8@gmail.com>
2022-10-18 10:13:50 +0600

    py conscptual 2-2 singly linked list insert at any pos

--- Conceptual/2-2 DS Implementation using Python/singlyLinkList.py
+++ Conceptual/2-2 DS Implementation using Python/singlyLinkList.py
@@ -1,3 +1,6 @@
+from operator import ne
+
+
 class Node:
     def __init__(self,v):
         self.val = v
@@ -6,8 +9,21 @@ class Node:
 class Linked_list:
     def __init__(self):
         self.head = None
-    def insert_at_postition(self,pos,val):
-        pass
+    def insert_at_pos(self,pos,val):
+        newNode = Node(val)
+        if pos == 0:
+            newNode.next = self.head
+            self.head = newNode
+        else:
+            tmp = self.head
+            for i in range(pos-1):
+                tmp = tmp.next
+                if tmp == None:
+                  print("Out of Bound")
+                  return  
+            newNode.next = tmp.next
+            tmp.next = newNode
+
 
     def insert_at_tail(self,val):
         newNode = Node(val)
@@ -30,7 +46,10 @@ def main():
     lst.insert_at_tail(10)
     lst.insert_at_tail(20)
     lst.insert_at_tail(30)
+    # lst.print_list()
+    lst.insert_at_pos(4,100)
     lst.print_list()
+
     # a = Node(10)
     # b = Node(23)
     # c = Node(30)
