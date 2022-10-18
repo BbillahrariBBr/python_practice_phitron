f4890a1624394682596b9d3f52f46676521b982e
BbillahrariBBr <mb.billah8@gmail.com>
2022-10-18 22:22:35 +0600

    q 09 done

--- Module-07LabAssignment01/q09.py
+++ Module-07LabAssignment01/q09.py
@@ -1,10 +1,20 @@
+def replace_word(lst, s):
+    l = len(lst)
+    for ind,word in enumerate(lst):
+        # print(word,index)
+        if ind+1 < l:
+            next_word = replace_with[ind+1] 
+            s = s.replace(word,next_word)
+            del replace_with[ind+1]
+            
+            # print(next_word)
+        else:
+            None
+        
+    return s
+
 
 replace_with = ["chief", "thief", "superintendent", "sweeper", "married", "left", "tried", "died"]
 s = "I am the chief of Baghdad. Before that I used to be a superintendent of Bank Asia. Things have changed a lot since Jorina married me. A lot of girls tried to marry me."
-print(s.find(replace_with[0]))
-# for i in replace_with:
-#     if(s.find(i)):
-#         print(i)
-
-# print(s)
-
+sa =  replace_word(replace_with,s)
+print(sa)
