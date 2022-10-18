ad72c9f4eda27b18be1ca373d22328da41a9b88d
BbillahrariBBr <mb.billah8@gmail.com>
2022-10-18 15:18:25 +0600

    lab xm 07 done

---
+++ Module-07LabAssignment01/q07.py
@@ -0,0 +1,13 @@
+def replace_comma_with_space(text):
+    output = ""
+    for i in text:
+        if i == ',':
+            i = ' '
+        output += i
+   
+    return output
+
+s = "I,have,been,practising,python,since,the,course,started"
+
+output = replace_comma_with_space(s)
+print(output)
