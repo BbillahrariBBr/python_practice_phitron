3c69632ee6ef773804304fc3a13760113f4ff7b4
BbillahrariBBr <mb.billah8@gmail.com>
2022-10-19 18:47:33 +0600

    print(my_laptop.__dict__)

---
+++ Module-08Class/class_attr.py
@@ -0,0 +1,16 @@
+class Shop:
+    cart = []
+
+    def __init__(self,buyer):
+        self.buyer = buyer
+
+    def add_to_cart(self,item):
+        self.cart .append(item)
+
+shopper_1 = Shop('Alu khan')
+shopper_1.add_to_cart('tshirt')
+print(shopper_1.cart)
+
+shopper_2 = Shop('Chanachur Khan')
+shopper_2.add_to_cart('Shoes')
+print(shopper_2.cart)
