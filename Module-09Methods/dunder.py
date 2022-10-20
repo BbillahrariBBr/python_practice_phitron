aa0676183d5f4cc29c7ab525fe589ff684637e70
BbillahrariBBr <mb.billah8@gmail.com>
2022-10-20 21:35:36 +0600

    9-2 add you own operator and magic methods

--- Module-09Methods/dunder.py
+++ Module-09Methods/dunder.py
@@ -2,10 +2,20 @@
 # magic
 # special methods
 class Person:
-    def __init__(self, name, age, money) -> None:
+    def __init__(self, name, age, money,height =65) -> None:
         self.name = name
         self.age = age
         self.money = money
+        self.height = height
+    
+    def __call__(self):
+        print( f'this is {self.name} with age {self.age} and have {self.money}')
+    
+    def __eq__(self,other):
+        return self.age == other.age
+    
+    def __len__(self):
+        return self.height
     def __add__(self,other):
         return self.money+other.money
 
@@ -13,4 +23,10 @@ alim = Person('Alim',15,560)
 dalim = Person('Dalim',16,750)
 print(alim+dalim)
 x = 5
+alim()
+
+print(alim == dalim)
+
+friends = [45,65,98,12,32]
+print(len(alim))
     
