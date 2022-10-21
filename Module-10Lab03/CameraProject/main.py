6a261f0613983733ba7b8ac6bd938b4620467cb1
BbillahrariBBr <mb.billah8@gmail.com>
2022-10-21 15:03:39 +0600

    10-6 Click

--- Module-10Lab03/CameraProject/main.py
+++ Module-10Lab03/CameraProject/main.py
@@ -27,6 +27,9 @@ class Window(QWidget):
         self.img_width = 640
         self.img_height = 400
 
+        # load icon
+        self.camera_icon = QIcon(cap_icon_path)
+
         # setup the window
         self.setWindowTitle("My Camera App")
         self.setGeometry(100,100,self.window_width,self.window_height)
@@ -41,13 +44,28 @@ class Window(QWidget):
 
     def ui(self):
         """ Contains all ui things """
+        # layout 
+        grid = QGridLayout()
+        self.setLayout(grid)
+
         # image label
         self.image_label = QLabel(self)
         self.image_label.setGeometry(0,0, self.img_width, self.img_height)
 
+        # capture button
+        self.capture_btn = QPushButton(self)
+        self.capture_btn.setIcon(self.camera_icon)
+        self.capture_btn.setStyleSheet("border-radius: 30; border: 2px solid black; border-width: 3px")
+        self.capture_btn.setFixedSize(60,60)
+        self.capture_btn.clicked.connect(self.save_img)
+
         if not self.timer.isActive():
             self.cap = cv2.VideoCapture(0)
             self.timer.start(20)
+
+        # add things to the layout
+        grid.addWidget(self.capture_btn,0,0)
+        grid.addWidget(self.image_label,0,1)
 
         self.show()
 
@@ -64,13 +82,15 @@ class Window(QWidget):
 
     def save_img(self):
         """ Save image from camera """
-        pass
+        print("Saving Image")
+        cv2.imwrite("my_img.jpg", self.frame)
 
     def record(self):
         """ record Video """
         pass
 
 #  run
+cap_icon_path = 'assets/capture.png'
 app  = QApplication(sys.argv)
 win = Window()
 sys.exit(app.exec_())
