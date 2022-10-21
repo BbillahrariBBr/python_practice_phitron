2fcff0c3ba594e31266f588563af2e7d3081594f
BbillahrariBBr <mb.billah8@gmail.com>
2022-10-22 00:03:48 +0600

    q02 done

---
+++ Module-11TheoryAssignment01/q02.py
@@ -0,0 +1,6 @@
+num1,num2,num3 = input("enter numbers: ").split("-")
+num1_float  = float(num1)
+num2_float = float(num2)
+num3_float = float(num3)
+res = num1_float+num2_float+num3_float
+print(f'sum is: {res}')
