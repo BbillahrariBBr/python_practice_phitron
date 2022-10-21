eada6e26c3fe98675fb2cc6e432fb935410c81c9
BbillahrariBBr <mb.billah8@gmail.com>
2022-10-21 11:54:01 +0600

    10-5 Read Camera

--- Module-10Lab03/CameraProject/main.py
+++ Module-10Lab03/CameraProject/main.py
@@ -9,6 +9,7 @@ import sys
 from turtle import window_width
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QPixmap, QImage, QIcon
+from PyQt5.QtCore import QTimer
 import cv2
 
 
@@ -31,6 +32,10 @@ class Window(QWidget):
         self.setGeometry(100,100,self.window_width,self.window_height)
         self.setFixedSize(self.window_width,self.window_height)
 
+        # setup timer
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.update)
+
         self.ui()
 
 
@@ -39,11 +44,23 @@ class Window(QWidget):
         # image label
         self.image_label = QLabel(self)
         self.image_label.setGeometry(0,0, self.img_width, self.img_height)
+
+        if not self.timer.isActive():
+            self.cap = cv2.VideoCapture(0)
+            self.timer.start(20)
+
         self.show()
 
     def update(self):
         """ Update Frame """
-        pass
+        _,self.frame = self.cap.read()
+
+        frame = cv2.cvtColor(self.frame, cv2.COLOR_BGR2RGB) # open cv always B GB read kore amdr drkre RGB
+        height,width,channel = frame.shape
+        step = channel*width
+
+        q_frame = QImage(frame.data, width, height, step,QImage.Format_RGB888)
+        self.image_label.setPixmap(QPixmap.fromImage(q_frame))
 
     def save_img(self):
         """ Save image from camera """
