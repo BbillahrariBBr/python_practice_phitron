8c3b91c4e539b4a4bf36c72a1fe8250bd54817db
BbillahrariBBr <mb.billah8@gmail.com>
2022-10-22 07:50:00 +0600

    q07 skeleton done

---
+++ Module-11TheoryAssignment01/q07.py
@@ -0,0 +1,11 @@
+# def powerset(s):
+#     x = len(s)
+#     newList = []
+#     for i in range(1 << x):
+#         newList.append ([s[j] for j in range(x) if (i & (1 << j))])
+#     return newList
+
+# output = powerset([4,5,6])
+# print(output)
+
+print(1<<3)
