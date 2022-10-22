b637ce0d5b516a70dd43ec051e60333d715d9ef1
BbillahrariBBr <mb.billah8@gmail.com>
2022-10-22 21:14:17 +0600

    07 done

--- Module-11TheoryAssignment01/q07.py
+++ Module-11TheoryAssignment01/q07.py
@@ -1,11 +1,11 @@
-def powerset(s):
+def subsets(s):
     x = len(s)
     newList = []
-    for i in range(1 << x):
+    for i in range(2 ** x):
         newList.append ([s[j] for j in range(x) if (i & (1 << j))])
     return newList
 
-output = powerset([4,5,6])
+output = subsets([4,5,6])
 print(output)
 
 # print(1<<3)
