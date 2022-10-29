a5d32076230b93201f1226193c51e17b5f4b290a
BbillahrariBBr <mb.billah8@gmail.com>
2022-10-29 21:05:48 +0600

    13-7 Introduction to Python method overriding and operator overloading

---
+++ Module-13PrincipleofOop/overloading.py
@@ -0,0 +1,24 @@
+# print(max(12,45,87,98,45,63,458,-8))
+# print(max([12,45,87,98,45,63,458,-8]))
+# print(max('A','P','K','C'))
+
+
+def add(num1,num2):
+    return num1+num2
+
+print(add(12,13))
+# print(add(12,13,50))
+
+# op overlaoding
+class Account:
+    def __init__(self,holder,balance) -> None:
+        self.holder = holder
+        self.__balance = balance
+    
+    def __add__(self,other):
+        return self.__balance + other.__balance
+
+my_acc = Account('Shakib',6000)
+her_acc = Account('shishir',90000)
+res = my_acc+her_acc
+print(res)
